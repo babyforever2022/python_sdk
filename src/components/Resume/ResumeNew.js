a37d8b3831b832f64d3958543c55c7c055560c90
makpushkin <makpushkin1224@gmail.com>
2023-04-14 00:10:05 +0900

    resume update and create

---
+++ src/components/Resume/ResumeNew.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from "react";
+import { Container, Row } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Particle from "../Particle";
+import pdf from "../../Assets/NiclasKato.pdf";
+import { AiOutlineDownload } from "react-icons/ai";
+import { Document, Page, pdfjs } from "react-pdf";
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+
+const resumeLink = "https://raw.githubusercontent.com/goldpal/myresume/main/NiclasKato.pdf";
+
+function ResumeNew() {
+  const [width, setWidth] = useState(1200);
+
+  useEffect(() => {
+    setWidth(window.innerWidth);
+  }, []);
+
+  return (
+    <div>
+      <Container fluid className="resume-section" style={{
+        minHeight: window.innerHeight - 10
+      }}>
+        <Particle />
+        <Row style={{ justifyContent: "center", position: "relative" }}>
+          <Button variant="primary" href={pdf} target="_blank">
+            <AiOutlineDownload />
+            &nbsp;Download CV
+          </Button>
+        </Row>
+
+        <Row className="resume" style={{overflow: "hidden"}}>
+          <Document file={pdf}>
+            <Page pageNumber={1} scale={width > 786 ? 1.8 : 0.6} />
+            <Page pageNumber={2} scale={width > 786 ? 1.8 : 0.6} />
+          </Document>
+        </Row>
+
+        <Row style={{ justifyContent: "center", position: "relative" }}>
+          <Button variant="primary" href={pdf} target="_blank">
+            <AiOutlineDownload />
+            &nbsp;Download CV
+          </Button>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default ResumeNew;
