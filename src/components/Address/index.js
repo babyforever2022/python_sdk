a4883e18d5c2f12cb802cb1eaae2fe70cc375b4c
makpushkin <makpushkin1224@gmail.com>
2023-04-17 12:09:29 +0900

    address add

---
+++ src/components/Address/index.js
@@ -0,0 +1,9 @@
+export const address = {
+    github: 'https://github.com/goldpal',
+    gmail: 'mailto:niclas.kato92@gmail.com',
+    outlookmail: 'mailto:niclaskato@outlook.com',
+    skype: 'https://join.skype.com/invite/weRQ1pwK17jc',
+    telegram: 'https://t.me/GoldPal777',
+    discord: 'https://discord.gg/w3V2Ee3Wg6',
+    linkedin: 'https://www.linkedin.com/in/nicholas-kato-27b830250/',
+}
